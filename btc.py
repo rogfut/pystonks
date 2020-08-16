7a9bcad1feefbebae3902cc0638ad50d1358183d
rogfut <rogerfutrell@outlook.com>
2020-08-16 01:51:39 -0700

    add btc price feature

---
+++ btc.py
@@ -0,0 +1,22 @@
+import json
+from datetime import date
+
+import requests
+from prettytable import PrettyTable
+from requests_html import HTMLSession
+from bs4 import BeautifulSoup
+
+from os import path
+
+def get_btc_price(url):
+    session = HTMLSession()
+    r = session.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+    soup = BeautifulSoup(r.content, 'html.parser')
+    div = soup.find('div', class_="cmc-details-panel-price")
+    price = div.find('span').text.strip()
+    change = div.find('span', class_="cmc-details-panel-price__price-change").text.strip()
+    volume_parent = soup.find('ul', class_="cmc-details-panel-stats")
+    volume_li = volume_parent.findAll('li')[1]
+    volume = volume_li.find('span').text.strip()
+    data = [price, change, volume]
+    return data
--- main.py
+++ main.py
@@ -5,8 +5,12 @@ from dotenv import load_dotenv
 import os
 
 from ipo_scrape import get_ipo
+from btc import get_btc_price
 
 load_dotenv()
+
+# emojis
+backhand_index_pointing_right = u'\U0001F449'
 
 def ipo(bot, update):
     chat_id = update.message.chat_id
@@ -19,10 +23,18 @@ def ipo(bot, update):
     print(text)
     bot.send_message(chat_id=chat_id, text=text, parse_mode='html')
 
+def btc(bot, update):
+    chat_id = update.message.chat_id
+    btc_data = get_btc_price(os.getenv('BTC_PRICE_URL'))
+    text = "BTC:  " + btc_data[0] + "\nChange:  " + btc_data[1] + "\nVol(24h):  " + btc_data[2] + "\n\nFollow" + backhand_index_pointing_right + "[@rogut](https://github.com/rogfut)"
+    bot.send_message(chat_id=chat_id, text=text, disable_web_page_preview='true', parse_mode='markdown')
+
+
 def main():
     updater = Updater(os.getenv('TELEGRAM_API_KEY'))
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('ipo', ipo))
+    dp.add_handler(CommandHandler('btc', btc))
     updater.start_polling()
     updater.idle()
 
